8ed3de903930521ba0f7f8e9c4d2586d224f9a6d
mubeen <mubeen1519@gmail.com>
2023-03-29 16:50:52 +0500

    multi-language support

---
+++ app/src/main/java/com/example/uptodo/screens/settings/DataStoreRepository.kt
@@ -0,0 +1,9 @@
+package com.example.uptodo.screens.settings
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+
+
+class DataStoreRepository(context: Context) {
+}
---
+++ app/src/main/java/com/example/uptodo/screens/settings/SettingViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.uptodo.screens.settings
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor() {
+}
